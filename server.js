4459f6b05840369649f725e30b2b9a9c3c3a92a8
s.pera6@gmail.com <s.pera6@gmail.com>
2021-12-22 15:49:05 -0700

    add several ejs and modify server.js

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const port = 3000
 // mount middleware
 app.use(morgan("dev"))
 app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(express.static('public'))
 
 // mount routes
 
@@ -28,6 +30,12 @@ app.get('/budgets/new', (req, res) => {
   res.render('new.ejs')
 })
 
+// post
+app.post('/budgets', (req, res) => {
+  budgets.push(req.body)
+  res.redirect('/budgets')
+})
+
 // create
 app.post('/budgets', (req, res) => {
   res.redirect('/budgets')
@@ -38,7 +46,12 @@ app.post('/budgets', (req, res) => {
 app.get('/budgets/:indexOfBudgetsArray', (req, res) => {
   res.render('show.ejs', {
     budget: budgets[req.params.indexOfBudgetsArray],
-    title: `${budgets[req.params.indexOfBudgetsArray].name} Show Page`
+    title: `${budgets[req.params.indexOfBudgetsArray].name}`,
+    date: `${budgets[req.params.indexOfBudgetsArray].date}`,
+    name: `${budgets[req.params.indexOfBudgetsArray].name}`,
+    from: `${budgets[req.params.indexOfBudgetsArray].from}`,
+    amount: `${budgets[req.params.indexOfBudgetsArray].amount}`,
+    tags: `${budgets[req.params.indexOfBudgetsArray].tags}`
   })
 })
 
